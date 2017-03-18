fa208d941d58eb83f4d4ff38165d6ddf0843679e
Pedro Martín <pmargom@gmail.com>
2017-03-18 20:35:58 +0000

    Added webapi endpoints mockup

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CarRental1._1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = new WebHostBuilder()
+                .UseKestrel()                       // web server cross-platform incluido por defecto al usar dotnet new
+                .UseStartup<Startup>();             // indicamos la clase que se usará como punto de entrada
+            
+            // la siguiente instrucción es opcional, es solo cambiar la ruta por defecto
+            // la ruta por defecto es: http://localhost:5000"
+            builder.UseUrls("http://localhost:8000");
+
+            // la siguiente instrucción es opcional. Sirve para cambiar el numero de threads o procesos
+            // que atenderán las peticiones. Permite destinar más recursos de CPU para anteder peticiones HTTP
+            builder.UseKestrel(options => {
+                options.ThreadCount = 4;
+            });
+
+            var host = builder.Build();
+            host.Run();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CarRental1._1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Indicamos que queremos usar las dependencias necesarias para hacer uso del framework MVC
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app) {
+
+            // Aquí se incluirán los uso de los diferentes middleware's que vaya
+            // necesitando nuestra aplicación
+            // El siguiente middleware es para indicar el tipo de rutas que se permitirán
+            // en los endpoints del api
+            app.UseMvc();
+
+        }
+    }
+}
